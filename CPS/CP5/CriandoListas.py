bafaab53b294dabae530ab5241c613cba39e2e97
Mmateus106 <rm98524@fiap.com.br>
2023-10-07 15:48:45 -0300

    Arquivo py que cria listas desordenadas em json

--- CPS/CP5/CriandoListas.py
+++ CPS/CP5/CriandoListas.py
@@ -1,3 +1,19 @@
 import json
 import random
 
+def criar_lista_num(lenght):
+    return [random.randint(1, 1000) for _ in range(lenght)]
+
+def salvar_json(nome_arquivo, lista):
+    with open(nome_arquivo, 'w') as arquivo:
+        json.dump(lista, arquivo)
+
+def criar_e_salvar_json(lenght):
+    lista = criar_lista_num(lenght)
+    nome_arquivo = f'Lista_{lenght}.json'
+    salvar_json(nome_arquivo, lista)
+    print(f'Arquivo {nome_arquivo} com lista desordenada gerada!')
+
+tamanhos = [10000, 100000, 500000, 1000000, 5000000]
+for lenght in tamanhos:
+    criar_e_salvar_json(lenght)
