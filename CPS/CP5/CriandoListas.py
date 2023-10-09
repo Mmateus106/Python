e3ba452ff49be34a9de7291e7daa42224cde13b3
Mmateus106 <rm98524@fiap.com.br>
2023-10-08 21:18:12 -0300

    Arquivo Criando listas arrumado.

--- CPS/CP5/CriandoListas.py
+++ CPS/CP5/CriandoListas.py
@@ -1,19 +1,19 @@
 import json
 import random
 
-def criar_lista_num(lenght):
-    return [random.randint(1, 10000) for _ in range(lenght)]
+def criar_lista_num(tamanho):
+    return [random.randint(1, 10000) for _ in range(tamanho)]
 
 def salvar_json(nome_arquivo, lista):
     with open(nome_arquivo, 'w') as arquivo:
         json.dump(lista, arquivo)
 
-def criar_e_salvar_json(lenght):
-    lista = criar_lista_num(lenght)
-    nome_arquivo = f'./CPS/CP5/Listas/Lista_{lenght}.json'
+def criar_salvar_json(tamanho):
+    lista = criar_lista_num(tamanho)
+    nome_arquivo = f'./CPS/CP5/Listas/Lista_{tamanho}.json'
     salvar_json(nome_arquivo, lista)
     print(f'Arquivo {nome_arquivo} com lista desordenada gerada!')
 
 tamanhos = [10000, 100000, 500000, 1000000, 5000000]
-for lenght in tamanhos:
-    criar_e_salvar_json(lenght)
+for tamanho in tamanhos:
+    criar_salvar_json(tamanho)
